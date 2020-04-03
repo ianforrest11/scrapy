90b978bf1307b41abb1a12b3ba4348c1bfbdfe6e
Ian Forrest <ianforrest11@gmail.com>
2020-04-03 00:41:55 -0400

    fixed formatting, cleaned repo

--- scrapy_test.py
+++ scrapy_test.py
@@ -9,16 +9,15 @@ class JobSpider(scrapy.Spider):
         'https://www.indeed.com/jobs?q=data+sci&l=New+York%2C+NY'+ str(page_number) +'',
     ]
 
-    def __init__(self):
-        self.ids_seen = set()
-
     def parse(self, response):
         for job in response.css('div.jobsearch-SerpJobCard'):
             yield {
                 'job_id': job.css('div').attrib['id'],
-                'job_title': job.css('a.jobtitle::text').extract(),
-                'company': job.css('span.company::text').extract(),
-                'location': job.css('div.accessible-contrast-color-location::text').extract(),
+                # 'job_title': job.css('a.jobtitle::text').extract(),
+                'job_title': job.css('a.jobtitle').attrib['title'],
+                'company': job.css('span.company a.turnstileLink::text').extract(),
+                # 'location': job.css('div.accessible-contrast-color-location::text').extract(),
+                'location': job.css('div.recJobLoc').attrib['data-rc-loc'],               
                 'rating': job.css('span.ratingsContent::text').extract(),
                 'date_posted': job.css('span.date::text').extract()
             }
@@ -28,23 +27,3 @@ class JobSpider(scrapy.Spider):
         if JobSpider.page_number < 1093:
             JobSpider.page_number += 10
             yield response.follow(next_page, callback = self.parse)
-            
-    def process_item(self, item, spider):
-        if item['job_id'] in self.ids_seen:
-            raise DropItem("Duplicate item title found: %s" % item)
-        else:
-            self.ids_seen.add(item['job_id'])
-            return item
-            
-            
-# class DuplicatesPipeline(object):
-
-#     def __init__(self):
-#         self.ids_seen = set()
-
-#     def process_item(self, item, spider):
-#         if item['id'] in self.ids_seen:
-#             raise DropItem("Duplicate item found: %s" % item)
-#         else:
-#             self.ids_seen.add(item['id'])
-#             return item
