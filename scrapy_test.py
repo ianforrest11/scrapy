64f637c7322e5bf8ca9245e3bd39e8664e1b7a2d
Ian Forrest <ianforrest11@gmail.com>
2020-04-02 23:30:04 -0400

    add

--- scrapy_test.py
+++ scrapy_test.py
@@ -1,12 +1,16 @@
 import scrapy
+from scrapy.exceptions import DropItem
 
 
 class JobSpider(scrapy.Spider):
     name = 'jobs'
     page_number = 0
     start_urls = [
-        'https://www.indeed.com/jobs?q=data+science&l=New+York,+NY&start='+ str(page_number) +'',
+        'https://www.indeed.com/jobs?q=data+sci&l=New+York%2C+NY'+ str(page_number) +'',
     ]
+
+    def __init__(self):
+        self.ids_seen = set()
 
     def parse(self, response):
         for job in response.css('div.jobsearch-SerpJobCard'):
@@ -24,3 +28,23 @@ class JobSpider(scrapy.Spider):
         if JobSpider.page_number < 1093:
             JobSpider.page_number += 10
             yield response.follow(next_page, callback = self.parse)
+            
+    def process_item(self, item, spider):
+        if item['job_id'] in self.ids_seen:
+            raise DropItem("Duplicate item title found: %s" % item)
+        else:
+            self.ids_seen.add(item['job_id'])
+            return item
+            
+            
+# class DuplicatesPipeline(object):
+
+#     def __init__(self):
+#         self.ids_seen = set()
+
+#     def process_item(self, item, spider):
+#         if item['id'] in self.ids_seen:
+#             raise DropItem("Duplicate item found: %s" % item)
+#         else:
+#             self.ids_seen.add(item['id'])
+#             return item
