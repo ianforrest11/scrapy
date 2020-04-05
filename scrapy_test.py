8b844aa980491dca7835717c6cfd58c53d41c03d
Ian Forrest <ianforrest11@gmail.com>
2020-04-05 01:33:03 -0400

    cleaup

--- scrapy_test.py
+++ scrapy_test.py
@@ -1,13 +1,16 @@
 # extracts info from
 
 import scrapy
+import re
 
 
 class JobSpider(scrapy.Spider):
     name = 'jobs'
     page_number = 0
+    job_title = 'cleaner'
+    location = ''
     start_urls = [
-        'https://www.indeed.com/jobs?q=data+sci&l=New+York%2C+NY'+ str(page_number) +'',
+        'https://www.indeed.com/jobs?q='+ job_title.replace(" ", "+") +'&l='+ location.replace(" ", "+") +'&start='+ str(page_number) +'',
     ]
 
     def parse(self, response):
@@ -15,16 +18,23 @@ class JobSpider(scrapy.Spider):
             yield {
                 'job_id': job.css('div').attrib['data-jk'],
                 'job_title': job.css('a.jobtitle').attrib['title'],
-                'company': job.css('span.company a.turnstileLink::text').extract(),
+                'company': job.css('span.company a.turnstileLink::text').get(),
                 'location': job.css('div.recJobLoc').attrib['data-rc-loc'],
-                'salary': job.css('span.salaryText::text').extract(),               
-                'rating': job.css('span.ratingsContent::text').extract(),
-                'date_posted': job.css('span.date::text').extract(),
+                'salary': job.css('span.salaryText::text').get(),               
+                'rating': job.css('span.ratingsContent::text').get(),
+                'job_desc': re.sub('<[^<]+?>', ' ', ''.join(job.css('div.summary li').getall())),
+                'date_posted': job.css('span.date::text').get(),
                 'link': 'www.indeed.com{}'.format(job.css('a.jobtitle').attrib['href'])
             }
         
+        next_page = response.css('div.pagination')
+        next_page = next_page.css('a::attr(href)')[5].get()
+        print('NEXT PAGEEEEEEEEEEE', next_page)
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
         
-        next_page = 'https://www.indeed.com/jobs?q=data+science&l=New+York,+NY&start='+ str(JobSpider.page_number) +''
-        if JobSpider.page_number < 1093:
-            JobSpider.page_number += 10
-            yield response.follow(next_page, callback = self.parse)
+        # next_page = 'https://www.indeed.com/jobs?q='+ job_title.replace(" ", "+") +'&start='+ str(JobSpider.page_number) +''
+        # if JobSpider.page_number < 1093:
+        #     JobSpider.page_number += 10
+        #     yield response.follow(next_page, callback = self.parse)
