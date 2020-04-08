94d97a7aeba198a8dcba518c30be90e1e43afb30
Ian Forrest <ianforrest11@gmail.com>
2020-04-08 15:58:30 -0400

    push updates

--- tutorial/spiders/linkedin_spider.py
+++ tutorial/spiders/linkedin_spider.py
@@ -1,12 +1,10 @@
-# extracts info from
-
 import scrapy
 import re
 from ..items import JobItem
 
 
 class JobSpider(scrapy.Spider):
-    name = 'indeed'
+    name = 'linkedin'
     job_title = ''
     locations = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL',
                'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA',
@@ -16,7 +14,7 @@ class JobSpider(scrapy.Spider):
     start_urls = []
     
     for location in locations:
-        url = 'https://www.indeed.com/jobs?q='+ job_title.replace(" ", "+") +'&l='+ location.replace(" ", "+") +'&start=0'
+        url = 'https://www.linkedin.com/jobs/search?keywords='+ job_title.replace(" ", "+") +'&location='+ location.replace(" ", "+") +'&trk=homepage-basic_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0'
         start_urls.append(url)
 
     def parse(self, response):
@@ -24,24 +22,24 @@ class JobSpider(scrapy.Spider):
         items = JobItem()
         
         #scrape info from website
-        for job in response.css('div.jobsearch-SerpJobCard'):
+        for job in response.css('li.result-card'):
             
             # set up string for key word analysis
-            job_title_str = job.css('a.jobtitle').attrib['title']
-            job_desc_str = re.sub('<[^<]+?>', ' ', ''.join(job.css('div.summary li').getall()))
+            job_title_str = job.css('h3.result-card__title::text').get()
+            job_desc_str = job.css('p.job-result-card__snippet').get()
             job_string = job_title_str + job_desc_str
             key_words = ['Intern', 'Entry Level', 'Entry-Level', 'Junior', 'Grad']
             
             if any(x in job_string for x in key_words):
-                job_id =  job.css('div').attrib['data-jk'],
-                job_position = job.css('a.jobtitle').attrib['title'],
-                company_name = job.css('span.company a.turnstileLink::text').get(),
-                job_location = job.css('div.recJobLoc').attrib['data-rc-loc'],
-                job_salary = job.css('span.salaryText::text').get(),
-                job_description = re.sub('<[^<]+?>', ' ', ''.join(job.css('div.summary li').getall())),
-                published_at = job.css('span.date::text').get(),
-                application_link = 'www.indeed.com{}'.format(job.css('a.jobtitle').attrib['href'])
-                source = 'Indeed.com'
+                job_id = '' #job.css('div').attrib['data-jk'],
+                job_position = job.css('h3.result-card__title::text').get(),
+                company_name = '' #job.css('span.company a.turnstileLink::text').get(),
+                job_location = '' #job.css('div.recJobLoc').attrib['data-rc-loc'],
+                job_salary = '' #job.css('span.salaryText::text').get(),
+                job_description = job.css('p.job-result-card__snippet').get(),
+                published_at = '' #job.css('span.date::text').get(),
+                application_link = '' #'www.indeed.com{}'.format(job.css('a.jobtitle').attrib['href'])
+                source = 'LinkedIn.com'
                 
                 #convert to item object
                 items['job_id'] = job_id
