7f631d3534310bf9b1dd81ddd1c764b9f7a75dbf
Ian Forrest <ianforrest11@gmail.com>
2020-04-28 12:47:11 -0400

    push monster to git

--- tutorial/spiders/monster_spider.py
+++ tutorial/spiders/monster_spider.py
@@ -9,29 +9,27 @@ from scrapy.loader.processors import TakeFirst
 
 class MonsterSpider(scrapy.Spider):  
     name = 'monster'
-    locations = 'NY' #['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL',
-    #             'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA',
-    #             'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE',
-    #             'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI',
-    #             'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']   
-    start_urls = ['https://www.monster.com/jobs/search/?where=&stpage=1&page=%s' % page for page in range(1,100)]
-    
-    # for location in locations:
-    #     url = ('https://www.monster.com/jobs/search/?where='+ location.replace(" ", "+") +'&stpage=0&page=%s' % page for page in xrange(1,54))
-    #     start_urls.append(url)   
+
+    start_urls = []
+    jobs = ['Intern', 'Entry-Level', 'Junior', 'Grad', 'Associate', 'Assistant', 'Staff']
+    locations = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL',
+                'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA',
+                'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE',
+                'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI',
+                'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
+    for job in jobs:
+        for i in range(1,100):
+            for location in locations:
+                url = ('https://www.monster.com/jobs/search/?q={}&where={}&stpage=1&page={}'.format(job, location, i))
+                start_urls.append(url)
+            
 
 
     def parse(self, response):
         #scrape info from website
         for job in response.css('section.card-content'):
 
-            # TODO set up string for key word analysis
-            job_title_str = job.css('a::text').get()
-            key_words = ['Intern', 'Entry Level', 'Entry-Level', 'Junior', 'Grad', 'Associate', 'Assistant', 'Staff']
-
-            if any(x in job_title_str for x in key_words):
                 l = JobLoader(item=JobItem(), selector=job)
-                #l.add_xpath('job_id','')
                 l.add_css('job_id', 'section.card-content::attr(data-jobid)')
                 l.add_css('job_id', 'h2.title a::attr(href)')
                 l.add_css('job_position', 'a::text')
@@ -47,11 +45,3 @@ class MonsterSpider(scrapy.Spider):
                 it = l.load_item()
 
                 yield it
-
-
-        # # TODO fix pagination
-        # next_page = response.css('a.mux-btn::attr(href)').get()
-        # #next_page = next_page.css('a.mux-btn::attr(href)').get()
-        # if next_page is not None:
-        #     #next_page = response.follow(next_page)
-        #     yield scrapy.Request(next_page, callback=self.parse)
